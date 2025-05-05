a00defd5e7ae99bb24b95eeeb4c2fe209bccc81e
Nursultan <97668069+qwavy@users.noreply.github.com>
2025-05-05 20:32:53 +0500

    style: header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,10 +1,3 @@
-import { fetchProjectDetails } from '../../../shared/api/api';
-import { useEffect } from 'react';
-
 export const Header = () => {
-  useEffect(() => {
-    fetchProjectDetails().then((res) => console.log(res));
-  });
-
   return <h1>Header</h1>;
 };
