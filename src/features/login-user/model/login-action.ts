7335f71655fe71e285ff4cc18a112da0d58926f5
vasiliyalex <vasiliyalexsandrovich@gmail.com>
2025-05-20 01:43:40 +0500

    feat: add user persistence in MobX store

--- src/features/login-user/model/login-action.ts
+++ src/features/login-user/model/login-action.ts
@@ -1,9 +1,10 @@
+import { userStore } from '@entities/user/model/user-store';
 import { buildCustomerClient } from '@shared/api/client/build-customer-client';
 
 export const loginAction = async (email: string, password: string) => {
   try {
     const { customer, accessToken, refreshToken } = await buildCustomerClient({ email: email, password: password });
-    console.log(customer, accessToken, refreshToken);
+    userStore.setUser(customer);
 
     return {
       success: true,
